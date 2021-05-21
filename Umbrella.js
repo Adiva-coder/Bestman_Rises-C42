a1b6210445762af68ecbe5d63e761404e8eb8783
Adiva-coder <76836516+Adiva-coder@users.noreply.github.com>
2021-05-21 11:48:11 +0530

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -4,7 +4,7 @@ class Umbrella {
             isStatic: true,
         }
         this.image = loadAnimation("walking_1.png", "walking_2.png", "walking_3.png", "walking_4.png", 
-                                    "walking_5.png", "walking_6.png", "walking_7.png", "/walking_8.png");
+                                    "walking_5.png", "walking_6.png", "walking_7.png", "walking_8.png");
         this.umbrella = Bodies.circle(x,y,50,options);
         this.radius = 50;
         World.add(world, this.umbrella);
